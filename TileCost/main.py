067be79a79ca7e66e0bec7ee102b5c7794c799ac
Sarthak-Bhagat <sarth.bhagat@gmail.com>
2021-05-05 15:44:54 +0530

    Cost for WxH tiles

---
+++ TileCost/main.py
@@ -0,0 +1,8 @@
+def main():
+    cost = float(input(u"Cost per unit²: "))
+    width = float(input("meters of the width: "))
+    height = float(input("meters of the height: "))
+    print(u"The cost is: \u20B9"+f"{float(cost*width*height)}")
+
+if __name__ == "__main__":
+    main()
