3d64e6b4cf66ed4476a4f2268d20c1b4a75dc3c9
Dez <59145893+Sarthak-Bhagat@users.noreply.github.com>
2023-03-17 13:21:16 +0530

    Update main.py

--- Sieve/main.py
+++ Sieve/main.py
@@ -2,6 +2,7 @@
 
 
 from tqdm import tqdm
+import time
 
 
 def basePrime(num):
@@ -16,6 +17,17 @@ def Prime(lst, num):
     """Create a list of prime numbers upto num."""
     return [i for i in lst if i % num != 0 or i == num]
 
+def sieve_of_eratosthenes(n):
+    primes = bytearray([True]) * (n+1)
+    primes[0] = primes[1] = False
+    
+    for i in range(2, int(n**0.5)+1):
+        if primes[i]:
+            sieve = bytearray([False]) * ((n-i*i)//i + 1)
+            primes[i*i:n+1:i] = sieve
+    
+    return [i for i in range(2, n+1) if primes[i]]
+
 
 primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
 L = int(100000)
@@ -28,3 +40,7 @@ for i in tqdm(sieve):
     sieve = Prime(sieve, i)
 
 print(sieve)
+
+start = time.perf_counter()
+print(sieve_of_eratosthenes(1000000))
+print(time.perf_counter()-start)
